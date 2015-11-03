688b70582656676361620e7d5336bad4fa09f693
t0mixcs <tomasp834@gmail.com>
2015-11-02 23:40:00 -0300

    Update borrador.py

--- borrador.py
+++ borrador.py
@@ -1,7 +1,6 @@
 #faltaria una manera de ver la dimension del mapa para Mapa(x,y), una seria recorrer el archivo del mapa 2 veces.
-#Para la dimension, estoy casi seguro, que Mapa(x,y) lo ponemos abajo despues de recorrer el mapa y agregar a los actores
-#con x,y, que van a seguir teniendo los mismos valores, no va a hacer falta recorrer el archivo dos veces
-
+#Para la dimension, le pregunte la longitud de filas(que vendria a ser el largo) y de filas[0](que vendria a ser el ancho)
+#Esto de la dimension ya lo pongo en el codigo original porque creo que es la mejor manera de obtener la dimension
 mapa = Mapa(x,y)
 x = 0
 y = 0
