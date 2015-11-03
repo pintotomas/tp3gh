fc56276aabfb554c7f58b768960a6f2165f0ae0f
t0mixcs <tomasp834@gmail.com>
2015-11-03 00:49:31 -0300

    Update borrador.py

--- borrador.py
+++ borrador.py
@@ -1,9 +1,11 @@
 #faltaria una manera de ver la dimension del mapa para Mapa(x,y), una seria recorrer el archivo del mapa 2 veces.
 #Para la dimension, le pregunte la longitud de filas(que vendria a ser el largo) y de filas[0](que vendria a ser el ancho)
 #Esto de la dimension ya lo pongo en el codigo original porque creo que es la mejor manera de obtener la dimension
+#Para hacer que quede mejor la asignacion 
 mapa = Mapa(x,y)
 x = 0
 y = 0
+representacion_actores = {"@":"Heroe"
 with open(nombre_mapa) as m:
 	for linea in m:
 		x = 0
