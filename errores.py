978dfe4bd16ed5a5bf6849eeeb437f877231e6b8
t0mixcs <tomasp834@gmail.com>
2015-11-04 20:05:47 -0300

    Create errores.py

---
+++ errores.py
@@ -0,0 +1,8 @@
+class MapaIncorrectoError(Exception):
+    pass
+class PersonajeInexistenteError(Exception):
+    pass
+class NoHayHeroeError(Exception):
+    pass
+class NoHaySalidaError(Exception):
+    pass
