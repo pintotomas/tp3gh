5ce192404fe60281fd62081317d7885108b30333
t0mixcs <tomasp834@gmail.com>
2015-11-06 21:46:06 -0300

    Update juego.py

--- juego.py
+++ juego.py
@@ -18,17 +18,41 @@ class Juego(object):
         # El historial de mensajes es simplemente una lista de strings.
         self.mensajes = []
         
+    def agregar_goblin(self,mapa,x,y):
+        "Agrega un goblin al mapa"
+        goblin = actores.Goblin()
+        mapa.agregar_actor(goblin,x,y)
+        
+    def agregar_orco(self,mapa,x,y):
+        "Agrega un orco al mapa"
+        orco = actores.Orco()
+        mapa.agregar_actor(orco,x,y)
+        
+    def agregar_pared(self,mapa,x,y):
+        "Agrega una pared al mapa"
+        pared = actores.Pared()
+        mapa.agregar_actor(pared,x,y)
+        
+    def agregar_salida(self,mapa,x,y):
+        "Agrega una salida al mapa"
+        salida = actores.Salida()
+        mapa.agregar_actor(salida,x,y)
+
+    
+        
     def crear_mapa(self, filas):
         """Crea el Mapa a partir de la definicion provista por parametro (`filas`),
         y devuelve una tupla con el mapa y el actor que representa al heroe del juego.
         Si hay mas de un heroe en la definicion del mapa, o bien si no hay ningun
-        heroe, la funcion lanza una excepcion.
+        heroe, la funcion lanza una excepcion. Se presupone que en el mapa se
+        proporciona una salida para poder finalizar el juego.
         `filas` es una lista de strings, que corresponde con el contenido completo
         del archivo .map."""
-        ####
-        #### Modificar este codigo para que cargue el mapa dinamicamente
-        #### a partir de `filas`
-        ####
+        dic_funciones = {"#":Juego.agregar_pared,
+                         "o":Juego.agregar_orco,
+                         "g":Juego.agregar_goblin,
+                         "<":Juego.agregar_salida
+                         }
         ANCHO = len(filas[0]) 
         if ANCHO == 0: raise errores.MapaIncorrectoError\
 		("Primer linea del archivo vacia")
@@ -37,7 +61,6 @@ class Juego(object):
         x = 0
         y = -1
         heroe = None
-        salida = None
         for fila in filas:
             if len(fila) != ANCHO: raise errores.MapaIncorrectoError\
            ("la linea",fila,\
@@ -46,33 +69,18 @@ class Juego(object):
             y+= 1
             for caracter in fila:
                 if caracter == "@":
-                   heroe = actores.Heroe()
-                   mapa.agregar_actor(heroe,x,y)
-                        
-                elif caracter == "#":
-                   pared = actores.Pared()
-                   mapa.agregar_actor(pared,x,y)
-                elif caracter == "g":
-                   goblin = actores.Goblin()
-                   mapa.agregar_actor(goblin,x,y)
-                        
-                elif caracter == "o":
-                   orco = actores.Orco()
-                   mapa.agregar_actor(orco,x,y)
-                        
-                elif caracter == "<":
-                   salida = actores.Salida()
-                   mapa.agregar_actor(salida,x,y)
+                    # Lo necesitamos ya que, debemos definir heroe para poder devolverlo
+                    heroe = actores.Heroe()
+                    mapa.agregar_actor(heroe,x,y)
+                elif caracter in dic_funciones:
+                    dic_funciones[caracter](self,mapa,x,y)
                 elif caracter != ".":
                    raise errores.PersonajeInexistenteError\
                    ("El caracter",caracter,"no hace referencia a ningun personaje en el juego")
-		   
-		   
             	x+=1
+            
         if heroe == None:
-            raise errores.NoHayHeroeError("El mapa provisto no contiene ningun heroe para ser controlado por el usuario")
-        if salida == None:
-            raise errores.NoHaySalidaError("El mapa provisto no provee una salida para el calabozo!")
+            raise errores.NoHayHeroeError("No hay ningun heroe(@) en el mapa")
         return mapa, heroe
 
     def turno(self, evento):
@@ -151,4 +159,4 @@ except IOError:
 except errores.PersonajeInexistenteError:
     print "El mapa provisto incluye personajes que no son del juego!."
 except errores.MapaIncorrectoError:
-    print "Formato del mapa incorrecto, debe ser de tamaño mxn"
+    print "Formato del mapa incorrecto, debe ser rectangular"
